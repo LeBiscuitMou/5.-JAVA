481c06d815f4acbbd72c1793c13d339b209039f3
LeBiscuitMou <59147867+LeBiscuitMou@users.noreply.github.com>
2023-10-25 15:21:01 +0200

    Initial commit

---
+++ 2. Exercices/demo/src/net/ent/etrs/Demo.java
@@ -0,0 +1,7 @@
+package net.ent.etrs;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Bonjour monde !\n");
+    }
+}
---
+++ 2. Exercices/helloWorld/src/net/entrs/ets/HelloWorld.java
@@ -0,0 +1,7 @@
+package net.entrs.ets;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
